8c9cef6748d450f19129480eca4d02b952223c61
Alohalong <90435137+Alohalong@users.noreply.github.com>
2022-07-11 20:54:25 +0700

    Add files via upload

---
+++ Exercise4_Sandusit_B.py
@@ -0,0 +1,10 @@
+found_eng = 88
+gene_bu = 79.5
+intro_com = 49.5
+comput = 100
+
+print("--- Your Score ---")
+print("Foundation English :",found_eng)
+print("General Business :",gene_bu)
+print("Introduction to Computer Systems :",intro_com)
+print("Computer Programming :",comput)
